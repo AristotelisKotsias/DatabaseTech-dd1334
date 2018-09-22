63f0bfc19e5c65a593f8f2c351d5c46af7f2b964
Aristotelis <kotsias.a@gmail.com>
2018-09-22 17:30:13 +0200

    first commit

---
+++ Lab1/queries.sql
@@ -0,0 +1,129 @@
+
+\echo *** Task 1.1 *** \echo
+
+SELECT
+  last_name,
+  first_name
+FROM authors
+WHERE author_id = (
+  SELECT author_id
+  FROM books
+  WHERE title = 'The Shining'
+);
+
+\echo *** Task 1.2 *** \echo
+
+SELECT title
+FROM books
+WHERE author_id = (
+  SELECT author_id
+  FROM authors
+  WHERE first_name = 'Paulette' AND last_name = 'Bourgeois'
+);
+
+\echo *** Task 1.3 *** \echo
+
+SELECT last_name, first_name
+FROM customers
+WHERE customer_id IN (
+	SELECT customer_id
+	FROM shipments
+	WHERE isbn IN (
+		SELECT isbn
+		FROM editions
+		WHERE book_id IN (
+			SELECT book_id
+			FROM books
+			WHERE subject_id IN (
+				SELECT subject_id
+				FROM subjects
+				WHERE subject = 'Horror'
+			)
+		)
+	)
+);
+
+\echo *** Task 1.4 *** \echo
+
+SELECT title
+FROM books
+WHERE book_id = (
+	SELECT book_id
+	FROM editions
+	WHERE isbn = (
+		SELECT isbn
+		FROM stock
+		WHERE stock = (					-- WHERE stock = max(stock) is forbidden because aggregate functions are not allowed in WHERE 
+			SELECT max(stock)
+			FROM stock
+		)												
+	)	
+);
+
+-\echo *** Task 1.4 (2d solution) *** \echo
+
+SELECT title
+FROM books
+	INNER JOIN editions ON books.book_id = editions.book_id
+	INNER JOIN stock ON editions.isbn = stock.isbn
+WHERE stock = ( 
+	SELECT max(stock)
+	FROM stock
+);	
+
+\echo *** Task 1.5 *** \echo
+
+SELECT SUM(retail_price)
+FROM stock
+	INNER JOIN editions ON stock.isbn = editions.isbn
+	INNER JOIN books ON editions.book_id = books.book_id 
+	INNER JOIN subjects ON books.subject_id = subjects.subject_id
+WHERE subject = 'Science Fiction';
+
+\echo *** Task 1.6 *** \echo
+
+SELECT title
+FROM books
+	INNER JOIN editions ON books.book_id = editions.book_id
+	INNER JOIN shipments ON editions.isbn = shipments.isbn
+GROUP BY books.title
+HAVING COUNT(customer_id) = 2;			-- The HAVING clause is used because the WHERE keyword cannot be used with aggregate functions --
+
+\echo *** Task 1.7 *** \echo
+
+--> TODO <--
+/*SELECT name, MAX(cost)
+FROM publishers
+	INNER JOIN editions ON publishers.publisher_id = editions.publisher_id
+	INNER JOIN stock ON editions.isbn = stock.isbn
+GROUP BY publishers.name ORDER BY SUM(cost) DESC LIMIT 1;*/
+
+\echo *** Task 1.8 *** \echo
+
+SELECT SUM(retail_price - cost)
+FROM shipments,stock
+WHERE shipments.isbn = stock.isbn;
+
+\echo *** Task 1.9 *** \echo
+
+SELECT last_name, first_name
+FROM customers
+	INNER JOIN shipments ON customers.customer_id = shipments.customer_id
+	INNER JOIN editions ON shipments.isbn = editions.isbn
+	INNER JOIN books ON editions.book_id = books.book_id
+	INNER JOIN subjects ON books.subject_id = subjects.subject_id
+GROUP BY customers.last_name, customers.first_name
+HAVING COUNT(subjects.subject_id) > 2;
+
+\echo *** Task 1.10 *** \echo
+
+SELECT subject
+FROM subjects
+WHERE subject NOT IN (
+	SELECT DISTINCT subject
+	FROM shipments, editions, books, subjects
+  	WHERE (shipments.isbn = editions.isbn
+        AND editions.book_id = books.book_id
+        AND books.subject_id = subjects.subject_id
+	)
+);
---
+++ Lab1/task2.sql
@@ -0,0 +1,108 @@
+
+\echo *** Task 2.1 *** \echo
+
+CREATE VIEW isbn_and_title AS
+  SELECT isbn, title
+  FROM editions, books
+  WHERE editions.book_id = books.book_id;
+DROP VIEW isbn_and_title;
+
+\echo *** Task 2.2 *** \echo
+
+INSERT INTO editions(isbn, book_id, edition, publisher_id, publication_date)
+VALUES ('5555', 12345, 1, 59, '2012-12-02');
+
+/*ERROR: insert or update on table "editions" violates foreign key constraint "editions_book_id_fkey"
+  DETAIL: Key (book_id)=(12345) is not present in table "books".
+  COMMENT: book_id must exist in books, before an editions of this book can be inserted*/
+
+\echo *** Task 2.3 *** \echo
+
+INSERT INTO editions(isbn)
+VALUES ('5555');
+
+/*ERROR: new row for relation "editions" violates check constraint "integrity"
+  DETAIL: Failing row contains (5555, null, null, null, null).
+  COMMENT: from create.sql it is stated that book_id and edition cannot be NULL*/
+
+\echo *** Task 2.4 *** \echo
+
+INSERT INTO books(book_id,title)
+VALUES (12345, 'How I Insert');
+INSERT INTO editions(isbn, book_id, edition, publisher_id, publication_date)
+VALUES ('5555', 12345, 1, 59, '2012-12-02');
+SELECT *
+FROM books;
+
+/*Author and subject can be NULL*/
+
+\echo *** Task 2.5 *** \echo
+/*SELECT *
+FROM subjects;*/
+
+UPDATE books
+SET subject_id = 10 -- subject_id = 10 refers to mystery subject 
+WHERE book_id = 12345;
+
+/*SELECT *
+FROM subjects;*/
+
+\echo *** Task 2.6 *** \echo
+
+DELETE FROM books
+WHERE book_id = 12345;
+
+/*ERROR: update or delete on table "books" violates foreign key constraint "editions_book_id_fkey" on table "editions"
+  DETAIL: Key (book_id)=(12345) is still referenced from table "editions".
+  COMMENT: book_id still exists in "editions" table, it has to be deleted in the right order from both tables*/
+
+\echo *** Task 2.7 *** \echo
+
+DELETE FROM editions
+WHERE book_id = 12345;
+DELETE FROM books
+WHERE book_id = 12345;
+SELECT *
+FROM books;
+
+\echo *** Task 2.8 *** \echo
+
+INSERT INTO books(book_id,title,subject_id) 
+VALUES(12345,'How I Insert',3443);
+/* ERROR: insert or update on table "books" violates foreign key constraint "books_subject_id_fkey"
+   DETAIL: Key (subject_id)=(3443) is not present in table "subjects".
+   COMMENT: subject_id must be first updated in the table "subjects" (if it doesnt exist already) and then in the "books" table*/
+
+\echo *** Task 2.9 *** \echo
+
+ALTER TABLE books ADD CONSTRAINT hasSubject
+	CHECK (subject_id IS NOT NULL);
+
+\echo A book can be inserted with NULL author_id and NOT NULL subject_id \echo
+
+INSERT INTO subjects(subject_id)
+VALUES(99);
+INSERT INTO books(book_id, title,subject_id)
+VALUES (12321,'test',99);
+SELECT *
+/*FROM subjects;
+SELECT *
+FROM books;*/
+
+DELETE FROM books
+WHERE subject_id = 99;
+DELETE FROM subjects
+WHERE subject_id = 99;
+
+/*SELECT *
+FROM subjects;
+SELECT *
+FROM books;*/
+
+
+\echo A book cannot be inserted with NULL subject_id \echo
+
+INSERT INTO books(book_id, title)
+VALUES (12321,'test');
+
+ALTER TABLE books DROP CONSTRAINT hasSubject;
