9c013af9f5e8ed1c82f45c8888759f1194c3cd99
Aristotelis <kotsias.a@gmail.com>
2018-09-23 13:48:29 +0200

    task 3 completed

--- Lab1/triggers.sql
+++ Lab1/triggers.sql
@@ -1,24 +1,58 @@
 DROP FUNCTION decstock() CASCADE;
 
+--Creating the trigger function
 CREATE FUNCTION decstock() RETURNS trigger AS $pname$
 	BEGIN
 		IF (SELECT stock
 		    FROM stock
 		    WHERE stock.isbn = NEW.isbn) = 0
-		THEN
-			RAISE EXCEPTION 'There is no stock to ship'
+			THEN RAISE EXCEPTION 'There is no stock to ship';
 		ELSE
 			UPDATE stock
 			SET stock = stock - 1
-			WHERE stock.isbn = new.isbn
+			WHERE stock.isbn = new.isbn;
 		END IF;
 		RETURN NEW;
 	END;
 
 $pname$ LANGUAGE plpgsql;
 
+--Creating the trigger
 CREATE TRIGGER stockTrigger
-AFTER INSERT ON shipments
-FOR EACH ROW 
-EXECUTE PROCEDURE decstock();
+	AFTER INSERT ON shipments
+	FOR EACH ROW 
+	EXECUTE PROCEDURE decstock();
 
+\echo *** Display stock table before making shipments *** 
+
+SELECT *
+FROM stock;
+
+\echo *** Ship a book that is not in stock *** 
+
+INSERT INTO shipments
+VALUES (2000, 860, '0394900014', '2012-12-07');
+
+\echo *** Ship a book that is in stock *** 
+
+INSERT INTO shipments
+VALUES (2001, 860, '044100590X', '2012-12-07'); 
+
+SELECT *
+FROM shipments
+WHERE shipment_id > 1999;
+
+\echo *** Display stock table ***
+
+SELECT *
+FROM stock;
+
+\echo *** Restote database to its original state ***
+
+DELETE FROM shipments
+WHERE shipment_id > 1999;
+UPDATE stock
+SET stock = 89
+WHERE isbn = '044100590X';
+
+DROP FUNCTION decstock() CASCADE;
